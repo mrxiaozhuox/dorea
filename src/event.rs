cb0f151cc0bc81f75a7dc4aa7b26211d03b1d044
mrxiaozhuox <mrxzx@wwsg18.top>
2021-08-25 18:32:40 +0800

    feat: event manager tool

--- src/event.rs
+++ src/event.rs
@@ -1,5 +1,20 @@
-use delay_timer::prelude::*;
+/// 事件系统控制器（Dorea DB）
 
-struct EventManager { /* 待编辑 */ }
+use tokio::time;
 
 
+#[allow(dead_code)]
+pub async fn loop_events () {
+    
+    let mut interval = time::interval(time::Duration::from_secs(1));
+
+    // 循环监听需要进行的任务
+    loop {
+        interval.tick().await;
+    }
+}
+
+// 用于合并已归档的数据库
+pub async fn merge_db() {
+ // 待编写...
+}
