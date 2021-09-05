fb67742510987f4f19eeb0dd2ad8c0941bcb49b1
mrxiaozhuox <mrxzx@wwsg18.top>
2021-09-05 21:26:08 +0800

    feat: start to change storage model

---
+++ examples/bulk.rs
@@ -0,0 +1,18 @@
+/// 在你运行这个 Demo 之前，请确保 Dorea 服务已经正常启动！
+
+use dorea::{client::DoreaClient, value::DataValue};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+
+    let mut db = DoreaClient::connect(("127.0.0.1", 3450), "").await?;
+
+    // db.select("demo").await?;
+
+    for i in 0.. {
+        db.setex(format!("KEY@{}", i).as_str(), DataValue::Number(i as f64), 0).await.unwrap();
+    }
+
+
+    Ok(())
+}
