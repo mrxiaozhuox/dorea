31587c52ab368ce91a57f02a302e92f8b3f4af3a
mrxiaozhuox <mrxzx@wwsg18.top>
2021-10-25 20:44:59 +0800

    feat: commit shell

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-cargo build --bin dorea-$1 && clear && ./target/debug/dorea-$1
+if [ $1 == "server" ];then
+    cargo build --bin dorea-$1 && clear && ./target/debug/dorea-$1
+elif [ $1 == "client" ];then
+    cargo build --bin dorea-$1 && clear && ./target/debug/dorea-$1
+else
+    ECHO "unknown operation: $1 [ server , client ]"
+fi
