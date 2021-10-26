903ce8663f959c6da426c2c0df0b1ece52691135
mrxiaozhuox <mrxzx@wwsg18.top>
2021-10-26 08:22:38 +0800

    feat: commit macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,1 +1,16 @@
 //! 相关宏程序等待更新中 ...
+
+/// use database_type to create a "DataValue"
+///
+/// - @Number -> 1
+/// - @String -> "hello world".to_string()
+/// - @Dict -> HashMap::new()
+/// - @Boolean -> false
+#[macro_export]
+macro_rules! database_type {
+    (
+        @$key:ident : $value:expr
+    ) => {
+        DataValue::$key($value)
+    };
+}
